c057aedd6896cf1e8b375ae2e4506baa94651d85
Sajjan <sajjan.maverick@gmail.com>
2025-03-29 01:52:43 +0530

    added changes

--- function.py
+++ function.py
@@ -1,12 +1,12 @@
 def add():
-
+    """This is demo"""
     print("This is sum function")
     list1 = [100, 10, 20, 30]
-    sum = 0
+    sum1 = 0
 
     for num in list1:
-        sum = sum + num
-    return sum
+        sum1 = sum1 + num
+    return sum1
 
 
 print("The sum of numbers in list : ", add())
